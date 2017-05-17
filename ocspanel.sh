f0042bb5445f26f465879d3fe3ddd69e704b621d
amadlovetia <amadloverara@gmail.com>
2017-05-18 00:43:49 +0700

    Update ocspanel.sh

--- ocspanel.sh
+++ ocspanel.sh
@@ -2,16 +2,12 @@
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
+clear
 echo "Selamat Datang Di Auto Installer OCS Debian7"
 echo "Masukkan Password Yang Anda Inginkan Sesudah Menekan ENTER"
 read -p "Tekan ENTER Untuk Melanjutkan"
 apt-get update && apt-get -y install mysql-server
 mysql_secure_installation
--n
--y
--y
--y
--y
 rm /etc/nginx/sites-enabled/default && rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
 mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup
@@ -23,13 +19,16 @@ useradd -m vps && mkdir -p /home/vps/public_html
 rm /home/vps/public_html/index.html && echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html && chmod -R g+rw /home/vps/public_html
 service php5-fpm restart && service nginx restart
+clear
 echo "Silahkan Buka Terlebih Dahulu Website: http://$IP:99/info.php"
-echo "Perhatian!!! Jika Sudah Membuka Website Nya, Langkah Selanjutnya Menekan ENTER"
+echo "Perhatian!!!"
 echo "Sesudah Menekan ENTER Akan Dimintai Password Yang Kamu Buat Sebelum Nya"
+echo "Dan Sesudah Memasukkan Password Akan Dimintai Memasukkan Kode, Dengan Awal Format: mysql>"
+echo "Dan Silahkan Kalian Salin Mantra Ini, Dan Isi Ke Format mysql>"
+echo "Mantra: CREATE DATABASE IF NOT EXISTS OCSPANEL;EXIT;"
 echo "Harap Isi Dengan Benar Ya...."
-read -p "Silahkan Tekan ENTER Jika Sudah Melakukan Perintah Diatas"
+read -p "Silahkan Tekan ENTER Jika Sudah Memahami Perintah Diatas"
 mysql -u root -p
--CREATE DATABASE IF NOT EXISTS OCSPANEL;EXIT;
 apt-get -y install git
 cd /home/vps/public_html
 git init
@@ -38,9 +37,10 @@ git pull origin master
 chmod 777 /home/vps/public_html/config
 chmod 777 /home/vps/public_html/config/config.ini
 chmod 777 /home/vps/public_html/config/route.ini
+clear
 echo "Buka Website Ini Terlebih Dahulu: http://$IP:99/"
 echo "Jika Sudah Silahkan Isi Database OCS Seperti Format Dibawah"
-echo "Bagian DATABASA Isi Seperti Ini: "
+echo "Bagian DATABASE Isi Seperti Ini: "
 echo "Database Host: localhost"
 echo "Database Name: OCSPANEL"
 echo "Database User: root"
@@ -51,8 +51,7 @@ echo "Passoword Baru: Sesuai Keinginan Mu"
 echo "Masukkan Ulang Password: Samakan Dengan Password Baru"
 read -p "Jika Sudah Instalasi Silahkan Tekan ENTER Untuk Melanjutkan"
 rm -R /home/vps/public_html/installation
-echo ""
-echo ""
+clear
 echo "Sekian Script Auto Installer OCS Panel Dari Sebastian Rahmad"
 echo "Saya Ucapkan Terima Kasih"
 echo "Silahkan Buka OCS Mu Yak, Isi Website Nya: http://$IP:99/"
